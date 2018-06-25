6222048b81ec6ebf8c41e948d300dddcb0f23ebf
Mary Mbugua <marymbugua.nm@gmail.com>
2018-06-26 00:52:02 +0300

    settings and urls to load landing page

--- soma/read/views.py
+++ soma/read/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def welcome(request):
+
+    return render(request,'index.html')
